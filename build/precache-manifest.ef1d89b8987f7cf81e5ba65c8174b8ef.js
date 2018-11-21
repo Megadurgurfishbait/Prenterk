43ee60c26fb5a98f2e7e5d920d37dc6e8df5428d
Megadurgurfishbait <gummehh@gmail.com>
2018-11-21 14:10:07 +0000

    build

---
+++ build/precache-manifest.ef1d89b8987f7cf81e5ba65c8174b8ef.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "9337f73b67c1ff4cb8c3a67a7f8a5e92",
+    "url": "/static/media/nafnspjald_2.9337f73b.JPG"
+  },
+  {
+    "revision": "576c9905688d2c14899a",
+    "url": "/static/css/main.822680d7.chunk.css"
+  },
+  {
+    "revision": "726524b5375530b002a6",
+    "url": "/static/js/1.726524b5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b491e2a39a45a886e723237fe0b60634",
+    "url": "/static/media/1-scs-printer-picture.b491e2a3.jpg"
+  },
+  {
+    "revision": "68d203d667274616e64e5e43d9c491d5",
+    "url": "/static/media/8806400_orig.68d203d6.jpg"
+  },
+  {
+    "revision": "1a96e1812e29b549bede43047eb142ce",
+    "url": "/static/media/17000226941_8e7f2ce7cc_k.1a96e181.jpg"
+  },
+  {
+    "revision": "40777fb2b9200ce4e3c7ca17316c1ba3",
+    "url": "/static/media/Digital-Design-720x392.40777fb2.jpg"
+  },
+  {
+    "revision": "571ee2372ce92a35add2c62230e7cf5a",
+    "url": "/static/media/tunnan-skirteini.571ee237.jpg"
+  },
+  {
+    "revision": "b32f3d9ac13ab2f27ebe6c5be5c45011",
+    "url": "/static/media/Umsl_g_.b32f3d9a.jpg"
+  },
+  {
+    "revision": "2d54672a347afdc554309ced3f4dffce",
+    "url": "/static/media/imageedit_1_3949203845.2d54672a.jpg"
+  },
+  {
+    "revision": "021dc7850788f3ded3a7f3673614b59d",
+    "url": "/static/media/Capture.021dc785.JPG"
+  },
+  {
+    "revision": "b74ddef5cd5a78076cc050a6b4360d8b",
+    "url": "/static/media/nafnspjald_1.b74ddef5.JPG"
+  },
+  {
+    "revision": "576c9905688d2c14899a",
+    "url": "/static/js/main.576c9905.chunk.js"
+  },
+  {
+    "revision": "b2e5939e8c42ceba9d48c83acbf540cf",
+    "url": "/static/media/nafnspjald_3.b2e5939e.JPG"
+  },
+  {
+    "revision": "509b87b030d62261704540d3468dc6f6",
+    "url": "/static/media/reikningur_1.509b87b0.JPG"
+  },
+  {
+    "revision": "9a2931f854f44bf1bf33f3375ea42b62",
+    "url": "/static/media/reikningur_2.9a2931f8.JPG"
+  },
+  {
+    "revision": "2e99b29e453eae5269f2c0cd0e82fc5c",
+    "url": "/static/media/reikningur_3.2e99b29e.JPG"
+  },
+  {
+    "revision": "ab8271a268b919f0589d6aa790110fd2",
+    "url": "/static/media/jolakort_1.ab8271a2.JPG"
+  },
+  {
+    "revision": "ec2cada8772c48febef577cc2a3763e8",
+    "url": "/static/media/jolakort_2.ec2cada8.JPG"
+  },
+  {
+    "revision": "f295e32b057f263b1700542d809ccbe4",
+    "url": "/static/media/jolakort_3.f295e32b.JPG"
+  },
+  {
+    "revision": "34ddef3a7a17e38b1a0fa3656b937ea8",
+    "url": "/static/media/hus.34ddef3a.JPG"
+  },
+  {
+    "revision": "dd69d9d6927cc2481486d0dc28d0954f",
+    "url": "/static/media/location.dd69d9d6.JPG"
+  },
+  {
+    "revision": "5e125998ddcb3df708d17cb957edabfe",
+    "url": "/static/media/prufael.5e125998.JPG"
+  },
+  {
+    "revision": "8c6ea4b4431f532533606a7390c95d19",
+    "url": "/static/media/logo.8c6ea4b4.jpg"
+  },
+  {
+    "revision": "398ecf8f1aa5f127a7b8186596e23bae",
+    "url": "/index.html"
+  }
+];
